8c50ec951d0bd9a99c19406a384258e601a30a59
theboysareher <ioshoadan5@gmail.com>
2026-06-16 19:31:40 +0100

    Initial commit

---
+++ Metadata.py
@@ -0,0 +1,53 @@
+from yt_dlp import YoutubeDL
+import json
+from uuid import uuid4
+from threading import Thread,Event
+
+class Youtube():
+    def __init__(self):
+        self.data = {}
+        self.event = Event()
+        
+    def get_metadata(self,url:str,callback):
+        self.thread = Thread(target=self._get,args=(url,callback))
+        self.thread.start()
+        
+    def save_metadata(self):
+        self.event.wait()
+        Thread(target=self._save).start()
+           
+    def _get(self,url:str,callback) -> str:
+       options = {"skip_download": True,"quiet":True,"no_warnings": True}    
+       with YoutubeDL(options) as ydl:
+           try:
+             info = ydl.extract_info(url, download=False)
+             data = {
+                "channel_name":info.get('channel',""),
+                "uploader": info.get('uploader_id',""),
+                "uploader_url":info.get('uploader_url',""),
+                "view_count":info.get('view_count',""),
+                "likes":info.get('like_count',""), 
+                "duration":f"{info.get('duration',"")}s",
+                "display_id":info.get('display_id',""),
+                "upload_date":(
+                        info['upload_date'][:4] + "/" +
+                        info['upload_date'][4:6] + "/" +
+                        info['upload_date'][6:]),
+               "channel_follower_count":info.get('channel_follower_count',""),
+               "channel_is_verified": info.get('channel_is_verified', False)}
+                
+              
+             self.data = data
+             callback(data)
+             self.event.set()                   
+           except Exception as e: print(e);self.event.set()
+           
+           
+    def _save(self) -> None:
+        try:
+           with open(f"{self.data['uploader']}-{uuid4().hex[:5]}.json","w") as f:
+               json.dump(self.data,f,indent=4)
+        except Exception as e :quit()
+           
+              
+      
---
+++ main.py
@@ -0,0 +1,59 @@
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from kivy.clock import Clock
+from kivymd.app import MDApp
+from Metadata import Youtube
+
+class App(MDApp):
+
+    def build(self):
+        self.yt = Youtube()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.material_style = "M3"
+
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(
+            Builder.load_file("Kv_ui/user_application.kv")
+        )
+
+        return screen_manager
+
+    def downloader(self,url):
+         self.yt.get_metadata(url, self.on_result)
+         
+    def on_result(self, data):
+     Clock.schedule_once(lambda dt: self.Data(data))
+     
+     
+    def Data(self,data):
+        screen = self.root.get_screen("main")
+        ids =  screen.ids
+        ids.channel_name.text = f"[+] channel_name: {data.get('channel_name', '')}"
+        ids.uploader.text = f"[+] uploader: {data.get('uploader', '')}"
+        ids.channel_is_verified.text = f"[+] channel_is_verified: {data.get('channel_is_verified', '')}"
+        ids.viecount.text = f"[+] viewcount: {data.get('view_count', '')}"
+        ids.likes.text = f"[+] likes: {data.get('likes', '')}"
+        ids.duration.text = f"[+] duration: {data.get('duration', '')}"
+        ids.display_id.text = f"[+] display_id: {data.get('display_id', '')}"
+        ids.upload_date.text = f"[+] upload_date: {data.get('upload_date', '')}"
+        ids.channel_follower_count.text = (f"[+] channel_follower_count: {data.get('channel_follower_count', '')}")
+
+    def Save_data(self):
+       self.yt.save_metadata()
+       dialog = MDDialog(
+             title="Debug",
+             text="Saved results to file Successfully !",
+             radius=[60,60,60,60],
+             buttons =
+             [MDFlatButton(text='Cancel',on_release= lambda x : dialog.dismiss())])           
+       dialog.open()
+                                 
+    def clear_field(self,field):
+         field.text = ""
+         
+
+if __name__ == "__main__":
+    App().run()
