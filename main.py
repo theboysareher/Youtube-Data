fd9bf374e306eef006e6bd4aa86b95eedcdd353d
theboysareher <ioshodan5@gmail.com>
2026-06-18 02:03:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ class App(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Green"
         self.theme_cls.material_style = "M3"
-        self.yt = Youtube()
-        return MDLabel(text="TEST")
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(
+            Builder.load_file("Kv_ui/user_application.kv")
+        )
+
+        return screen_manager
+
   
 
     def downloader(self,url):
